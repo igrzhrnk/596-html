81ef38a5c62a18287ad30e9e6b7e42048a97459a
igrzhrnk <igorzhrnko@gmail.com>
2021-05-26 18:26:51 +0300

    added new json file

--- js/restaurants.js
+++ js/restaurants.js
@@ -1,105 +1,30 @@
-const restaurantsJSON = 
-`[
-    "Salad Bar" : {
-        {
-            "id": "1",
-            "foodName" : "Turkey & Zucchini",
-            "foodPrice": "$25",
-            "foodDescription": "Green peppers, zucchini, spinach, pears, grana padano cheese, tomatoes, cucumbers, olives, sliced turkey breast."
-        },
-        {
-            "id": "2",
-            "foodName" : "Arugula Salad",
-            "foodPrice": "$35",
-            "foodDescription": "Arugula, raddichio, beets, cherry tomatoes, onions, balsamic vinegar, and grated Romano cheese."
-        },
-        {
-            "id": "3",
-            "foodName" : "Crab Salad",
-            "foodPrice": "$35",
-            "foodDescription": "Fresh Maryland jumbo crab meat over mixed greens, avocado, asparagus, and roasted peppers."
-        },
-        {
-            "id": "4",
-            "foodName" : "Antipasti Plate",
-            "foodPrice": "$37",
-            "foodDescription": "Green peppers, zucchini, spinach, pears, grana padano cheese, tomatoes, cucumbers, olives, sliced turkey breast."Roasted asparagus, smoked salmon, mushrooms, mixed olives, prosciutto, and gorgonzola cheese.
-        },
-        {
-            "id": "5",
-            "foodName" : "Sicilian Choice",
-            "foodPrice": "$45",
-            "foodDescription": "Fritter made with roasted chicken, pear tomatoes, avocado salad, and truffle vinaigrette."
-        },
-        {
-            "id": "6",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        }
-    },
-    "Pizza Italor": {
-        {
-            "id": "7",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "8",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "9",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "10",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "11",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        }
-    },
-    "Burger City": {
-        {
-            "id": "12",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "13",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "14",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "15",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
-        },
-        {
-            "id": "16",
-            "foodName" : "Vegan Wrap",
-            "foodPrice": "$25",
-            "foodDescription": "Mixed bell peppers, sun dried tomato spread, cucumbers, spinach, and fresh mozzarella."
+let restaurants;
+
+function loadingRestJSON() {
+    const restaurantId = document.querySelector('.tab-pane');
+    let htmlTemp = '';
+    for(restaurant of restaurants) {
+        htmlTemp += `
+            <div class="food-name">
+                <div class="food-values">
+                    <span class="group-description">${restaurant.foodName}</span>
+                    <hr>
+                    <span class="number-value">${restaurant.foodPrice}</span>
+                </div>
+            <span class="text-description">${restaurant.foodDescription}</span>
+            </div>
+        `;
+
+        if(restaurantId.id === restaurant.restaurantCode) {
+            restaurantId.innerHTML = `<div class="restaurant-deal">${htmlTemp}</div>`;
         }
     }
-]`;
+}
+
+async function loadRestaurants() {
+    const response = await fetch('../data/restaurants.json');
+    restaurants = await response.json();
+    loadingRestJSON();
+}
+
+loadRestaurants();
