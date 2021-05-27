98ecda75dc6d03ca597678f2f433ca2822edf925
igrzhrnk <igorzhrnko@gmail.com>
2021-05-27 19:24:24 +0300

    fix some error in restaurants.js

--- js/restaurants.js
+++ js/restaurants.js
@@ -1,29 +1,32 @@
 let restaurants;
 
 function loadingRestJSON(selector) {
-    const restaurantContainer = document.querySelector('#'+`${selector}`);
+    const restaurantContainer = document.querySelector('#' + selector);
     let htmlTemp = '';
-    for(const restaurant of restaurants) {
-        htmlTemp += `
-            <div class="food-name">
-                <div class="food-values">
-                    <span class="group-description">${restaurant.foodName}</span>
-                    <hr>
-                    <span class="number-value">${restaurant.foodPrice}</span>
-                </div>
-            <span class="text-description">${restaurant.foodDescription}</span>
-            </div>
-        `;
+    let oneRestaurantValue = {};
 
-        if(selector === restaurant.restaurantCode) {
-            restaurantContainer.innerHTML = `<div class="restaurant-deal">${htmlTemp}</div>`;
+    oneRestaurantValue = restaurants.filter(e => e.restaurantCode === selector);
+
+    for(const restaurant of oneRestaurantValue) {
+            htmlTemp += `
+                <div class="food-name">
+                    <div class="food-values">
+                        <span class="group-description">${restaurant.foodName}</span>
+                        <hr>
+                        <span class="number-value">${restaurant.foodPrice}</span>
+                    </div>
+                <span class="text-description">${restaurant.foodDescription}</span>
+                </div>
+            `;
         }
-    }
+
+    restaurantContainer.innerHTML = `<div class="restaurant-deal">${htmlTemp}</div>`;
 }
 
 async function loadRestaurants() {
     const response = await fetch('data/restaurants.json');
     restaurants = await response.json();
+
     loadingRestJSON('salad-bar');
     loadingRestJSON('pizza-italor');
     loadingRestJSON('burger-city');
