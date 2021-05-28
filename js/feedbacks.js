52468d57157727e0c753b57be62d110a7c321fba
igrzhrnk <igorzhrnko@gmail.com>
2021-05-28 18:15:06 +0300

    fix some error

--- js/feedbacks.js
+++ js/feedbacks.js
@@ -1,29 +1,34 @@
 (function() {
     let feedbacks;
 
-    function loadingFeedbackJSON() {
+    function loadingFeedbackJSON(sizeList) {
         const feedbackContainer = document.querySelector('.users-feedbacks .carousel-inner');
         let htmlTemp = '';
-        
         for(const feedback of feedbacks) {
-            htmlTemp += `
-                <div class="users-feedback-item">
-                    <div class="users-feedback-item-photo"><img src="${feedback.userImg}" alt="${feedback.userName}"></div>
-                    <span class="group-description users-feedback-title">${feedback.userName}</span>
-                    <span class="text-description">${feedback.feedbackText}</span>
-                    <span class="feedback-date">${feedback.feedbackDate}</span>
-                </div>
-            `;
+            for(let i = 1; i < sizeList; i++) {
+                if(+feedback.id === i) {
+                    htmlTemp += `
+                        <div class="col-md-6">
+                            <div class="users-feedback-item">
+                                <div class="users-feedback-item-photo"><img src="${feedback.userImg}" alt="${feedback.userName}"></div>
+                                <span class="group-description users-feedback-title">${feedback.userName}</span>
+                                <span class="text-description">${feedback.feedbackText}</span>
+                                <span class="feedback-date">${feedback.feedbackDate}</span>
+                            </div>
+                        </div>
+                `;
+                }
+            }
+           
         }
 
-
-        feedbackContainer.innerHTML = `<div class="carousel-item active"><div class="row"><div class="col-md-6">${htmlTemp}</div></div></div>`;
+        feedbackContainer.innerHTML = `<div class="carousel-item active"><div class="row">${htmlTemp}</div></div>`;
     }
     
     async function loadFeedbacks() {
         const response = await fetch('data/feedbacks.json');
         feedbacks = await response.json();
-        loadingFeedbackJSON();
+        loadingFeedbackJSON(3);
     }
 
     loadFeedbacks();
--- js/restaurants.js
+++ js/restaurants.js
@@ -1,35 +1,38 @@
-let restaurants;
+(function() {
+    let restaurants;
 
-function loadingRestJSON(selector) {
-    const restaurantContainer = document.querySelector('#' + selector);
-    let htmlTemp = '';
-    let oneRestaurantValue = {};
+    function loadingRestJSON(selector) {
+        const restaurantContainer = document.querySelector('#' + selector);
+        let htmlTemp = '';
+        let oneRestaurantValue = {};
 
-    oneRestaurantValue = restaurants.filter(e => e.restaurantCode === selector);
+        oneRestaurantValue = restaurants.filter(e => e.restaurantCode === selector);
 
-    for(const restaurant of oneRestaurantValue) {
-            htmlTemp += `
-                <div class="food-name">
-                    <div class="food-values">
-                        <span class="group-description">${restaurant.foodName}</span>
-                        <hr>
-                        <span class="number-value">${restaurant.foodPrice}</span>
+        for(const restaurant of oneRestaurantValue) {
+                htmlTemp += `
+                    <div class="food-name">
+                        <div class="food-values">
+                            <span class="group-description">${restaurant.foodName}</span>
+                            <hr>
+                            <span class="number-value">$${restaurant.foodPrice}</span>
+                        </div>
+                    <span class="text-description">${restaurant.foodDescription}</span>
                     </div>
-                <span class="text-description">${restaurant.foodDescription}</span>
-                </div>
-            `;
-        }
+                `;
+            }
 
-    restaurantContainer.innerHTML = `<div class="restaurant-deal">${htmlTemp}</div>`;
-}
+        restaurantContainer.innerHTML = `<div class="restaurant-deal">${htmlTemp}</div>`;
+    }
 
-async function loadRestaurants() {
-    const response = await fetch('data/restaurants.json');
-    restaurants = await response.json();
+    async function loadRestaurants() {
+        const response = await fetch('data/restaurants.json');
+        restaurants = await response.json();
 
-    loadingRestJSON('salad-bar');
-    loadingRestJSON('pizza-italor');
-    loadingRestJSON('burger-city');
-}
+        loadingRestJSON('salad-bar');
+        loadingRestJSON('pizza-italor');
+        loadingRestJSON('burger-city');
+    }
 
-loadRestaurants();
+    loadRestaurants();
+}());
+
