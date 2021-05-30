305219cc5ab319fdadceefaca22b4eced433db38
igrzhrnk <igorzhrnko@gmail.com>
2021-05-30 22:09:52 +0300

    fix some error

--- js/feedbacks.js
+++ js/feedbacks.js
@@ -2,13 +2,12 @@
     let feedbacks;
 
     function loadingFeedbackJSON() {
-        const feedbackContainer = document.querySelector('.users-feedbacks .carousel-inner');
+        const activeFeedbackContainer = document.querySelector('.users-feedbacks .carousel-inner>.carousel-item.active');
         let htmlTemp = '';
-        let currentItem = 1;
+
         for(const feedback of feedbacks) {
-            if(+feedback.id === currentItem) {
                 htmlTemp += `
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-12 col-sm-12 col-md-6">
                         <div class="users-feedback-item">
                             <div class="users-feedback-item-photo"><img src="${feedback.userImg}" alt="${feedback.userName}"></div>
                             <span class="group-description users-feedback-title">${feedback.userName}</span>
@@ -18,30 +17,19 @@
                     </div>
                 `;
             }
-
-            htmlTemp += `
-                    <div class="col-md-6 col-sm-12 clearfix d-none d-md-block">
-                        <div class="users-feedback-item">
-                            <div class="users-feedback-item-photo"><img src="${feedback.userImg}" alt="${feedback.userName}"></div>
-                            <span class="group-description users-feedback-title">${feedback.userName}</span>
-                            <span class="text-description">${feedback.feedbackText}</span>
-                            <span class="feedback-date">${feedback.feedbackDate}</span>
-                        </div>
-                    </div>
-                `;
-        }
-        
-        feedbackContainer.innerHTML = `<div class="carousel-item active"><div class="row">${htmlTemp}</div></div>`;
-        // feedbackContainer.innerHTML = `<div class="carousel-item"><div class="row">${htmlTemp}</div></div>`;
+            activeFeedbackContainer.innerHTML = `<div class="row">${htmlTemp}</div>`;
     }
+
+        
 
     async function loadFeedbacks() {
         const response = await fetch('data/feedbacks.json');
         feedbacks = await response.json();
+       
+        loadingFeedbackJSON();
         loadingFeedbackJSON();
     }
 
-    
     loadFeedbacks();
 
 }());
